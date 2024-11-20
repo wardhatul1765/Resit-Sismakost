5a1b6c4a1ce4ba3780032f5884520a16f4cfc05f
Miaw <hilmi.af05@gmail.com>
2024-11-20 14:11:02 +0700

    maps

--- PendopoLiving/index.php
+++ PendopoLiving/index.php
@@ -299,8 +299,13 @@ $total_mandi_luar = $data_mandi_luar['total_kamar'];
                   <p>kostelisa@gmail.com</p>
                 </div>
               </div><!-- End Info Item -->
-
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" frameborder="0" style="border:0; width: 100%; height: 270px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.0650512605586!2d113.7204238!3d-8.1638266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b7ef3d0d19%3A0x66cc368ecf37c79b!2sKOS%20ELISA!5e0!3m2!1sen!2sid!4v1700466219402!5m2!1sen!2sid" 
+                frameborder="0" 
+                style="border:0; width: 100%; height: 270px;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+              </iframe>
             </div>
           </div>
 
