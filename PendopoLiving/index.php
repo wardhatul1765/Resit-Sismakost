d0e904e6114d94ca86b0013e49ae1fd49f36fdfe
Miaw <hilmi.af05@gmail.com>
2024-11-21 14:53:59 +0700

    fix dropdown mobile

--- PendopoLiving/index.php
+++ PendopoLiving/index.php
@@ -299,12 +299,12 @@ $total_mandi_luar = $data_mandi_luar['total_kamar'];
                   <p>kostelisa@gmail.com</p>
                 </div>
               </div><!-- End Info Item -->
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.0650512605586!2d113.7204238!3d-8.1638266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b7ef3d0d19%3A0x66cc368ecf37c79b!2sKOS%20ELISA!5e0!3m2!1sen!2sid!4v1700466219402!5m2!1sen!2sid" 
-                frameborder="0" 
-                style="border:0; width: 100%; height: 270px;" 
-                allowfullscreen="" 
-                loading="lazy" 
-                referrerpolicy="no-referrer-when-downgrade">
+              <iframe src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.0650512605586!2d113.7204238!3d-8.1638266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b7ef3d0d19%3A0x66cc368ecf37c79b!2sKOS%20ELISA!5e0!3m2!1sen!2sid!4v1700466219402!5m2!1sen!2sid" 
+                frameborder = "0" 
+                style = "border:0; width: 100%; height: 270px;" 
+                allowfullscreen = "" 
+                loading = "lazy" 
+                referrerpolicy = "no-referrer-when-downgrade">
               </iframe>
             </div>
           </div>
